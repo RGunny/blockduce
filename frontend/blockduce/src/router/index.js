ae88c2e7a170403f0fbd8af2f3bce416116750c2
jua <bourzua@naver.com>
2021-03-12 18:23:07 +0900

    S04P22B107-53 [feat] : FE - 환경설정 추가

---
+++ frontend/blockduce/src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+// import Main from '../views/Main.vue'
+import Vote from '../views/After/Vote.vue'
+// import Wallet from '../views/After/Wallet.vue'
+// import Now from '../views/After/Now.vue'
+// import Blockduce from '../views/After/Blockduce.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  // {
+  //   path: '/',
+  //   name: 'Main',
+  //   component: Main
+  // },
+  {
+    path: '/after/vote',
+    name: 'Vote',
+    component: Vote
+  },
+  // {
+  //   path: '/after/wallet',
+  //   name: 'Wallet',
+  //   component: Wallet
+  // },
+  // {
+  //   path: '/after/now',
+  //   name: 'Now',
+  //   component: Now
+  // },
+  // {
+  //   path: '/after/blockduce',
+  //   name: 'Blockduce',
+  //   component: Blockduce
+  // },
+  
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
