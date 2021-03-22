cfa64d3c31dc70f9f470f69ca66571e7ba127ee6
Park Sangwoo <tkddnsos@gmail.com>
2021-03-22 22:58:08 +0900

    S04P23B107-48 [feat] : BE - Redis, Spring Security를 이용한 JWT Token 관리 추가

---
+++ backend/src/main/java/com/special/blockduce/config/JwtRequestFilter.java
@@ -0,0 +1,98 @@
+package com.special.blockduce.config;
+
+import com.special.blockduce.member.domain.Member;
+import com.special.blockduce.member.service.CustomUserDetailsService;
+import com.special.blockduce.utils.CookieUtil;
+import com.special.blockduce.utils.JwtUtil;
+import com.special.blockduce.utils.RedisUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private CookieUtil cookieUtil;
+
+    @Autowired
+    private RedisUtil redisUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException, IOException {
+
+        final Cookie jwtToken = cookieUtil.getCookie(httpServletRequest,JwtUtil.ACCESS_TOKEN_NAME);
+
+        String username = null;
+        String jwt = null;
+        String refreshJwt = null;
+        String refreshUname = null;
+
+        try{
+            if(jwtToken != null){
+                jwt = jwtToken.getValue();
+                username = jwtUtil.getUsername(jwt);
+            }
+            if(username!=null){
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+                if(jwtUtil.validateToken(jwt,userDetails)){
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                    usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                }
+            }
+        }catch (ExpiredJwtException e){
+            Cookie refreshToken = cookieUtil.getCookie(httpServletRequest,JwtUtil.REFRESH_TOKEN_NAME);
+            if(refreshToken!=null){
+                refreshJwt = refreshToken.getValue();
+            }
+        }catch(Exception e){
+
+        }
+
+        try{
+            if(refreshJwt != null){
+                refreshUname = redisUtil.getData(refreshJwt);
+
+                if(refreshUname.equals(jwtUtil.getUsername(refreshJwt))){
+                    UserDetails userDetails = userDetailsService.loadUserByUsername(refreshUname);
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                    usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+
+                    Member member = new Member();
+                    member.setName(refreshUname);
+                    String newToken =jwtUtil.generateToken(member);
+
+                    Cookie newAccessToken = cookieUtil.createCookie(JwtUtil.ACCESS_TOKEN_NAME,newToken);
+                    httpServletResponse.addCookie(newAccessToken);
+                }
+            }
+        }catch(ExpiredJwtException e){
+            //TODO : refreshJwt이 만료되었을 때 어떻게 할 것인가?
+        }
+
+        filterChain.doFilter(httpServletRequest,httpServletResponse);
+    }
+}
