0ba1196ae4bea3aa3f8d3e6b6a6736f04b6eef9e
Park Sangwoo <tkddnsos@gmail.com>
2021-04-01 10:28:44 +0900

    S04P23B107-111 [feat] : FE - 환경세팅 수정

--- frontend/server/routes/room.js
+++ frontend/server/routes/room.js
@@ -20,5 +20,22 @@ const rooms = [
 roomRouter.get('/', (req,res) => {
     res.send(rooms)
 })
+roomRouter.post('/room', (req, res) => {
+    const newRoom = req.body
+    if (!newUser.username) return res.send({ code: 400, message: 'Data is required' })
+    console.log(`room ${newRoom}`)
+    
+    // ChatRedis
+    //     .getUser(newUser.username, config.KEY)
+    //     .then(user => {
+    //         if (!user) {
+    //             ChatRedis.addUser(newUser.username, config.KEY, newUser)
+    //             console.log(`User ${newUser.username} logged`)
+    //             return res.send({ code: 200, message: 'Logged in succesfully' })
+    //         }
 
+    //         console.log(`User ${newUser.username} already exists!!`)
+    //         return res.send({ code: 401, message: 'Username already exists' })
+    //     })
+})
 module.exports = roomRouter
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -5,30 +5,25 @@
       <form novalidate class="md-layout" @submit.prevent="submitForm">
         <md-field>
           <label>Username</label>
-          <md-input 
-            v-model="username" 
-            type="string" 
-            id="username">
-          </md-input>
+          <md-input v-model="username" type="string" id="username"> </md-input>
         </md-field>
         <md-field>
           <label for="movie">Room</label>
           <md-select v-model="room" name="room" id="room">
-            <md-option 
-              v-for="room in rooms" 
-              :key="room.id" 
-              :value="room.name">{{room.name}}
+            <md-option v-for="room in rooms" :key="room.id" :value="room.name"
+              >{{ room.name }}
             </md-option>
           </md-select>
         </md-field>
         <div v-if="error" class="options-error">
-          <p>{{error}}</p>
+          <p>{{ error }}</p>
         </div>
         <div class="options__submit">
-          <md-button 
-            type="submit" 
-            class="md-raised md-primary" 
-            :disabled="!(username && room)">JOIN
+          <md-button
+            type="submit"
+            class="md-raised md-primary"
+            :disabled="!(username && room)"
+            >JOIN
           </md-button>
         </div>
       </form>
@@ -37,46 +32,47 @@
 </template>
 
 <script>
-import { STORE_ACTIONS } from "./../utils/config"
+import { STORE_ACTIONS } from './../utils/config';
 
 export default {
-  name: "home",
-  sockets:{
+  name: 'home',
+  sockets: {
     connect() {
-      this.$router.push("/chat")
-    }
+      this.$router.push('/chat');
+    },
   },
   data: () => ({
     username: undefined,
     room: undefined,
     rooms: [],
     error: undefined,
-    defaultError: 'Something went wrong' 
+    defaultError: 'Something went wrong',
   }),
   async created() {
     try {
-      this.rooms  = await this.$store.dispatch(STORE_ACTIONS.setRooms)
+      this.rooms = await this.$store.dispatch(STORE_ACTIONS.setRooms);
     } catch (error) {
-      this.error = this.defaultError
+      this.error = this.defaultError;
     }
   },
   methods: {
     async submitForm() {
-      if(!(this.username && this.room)) return
-      this.error = undefined
-      
+      if (!(this.username && this.room)) return;
+
+      this.error = undefined;
+
       try {
         await this.$store.dispatch(STORE_ACTIONS.joinRoom, {
           room: this.room,
-          username: this.username
-        })
-        this.$socket.open()
+          username: this.username,
+        });
+        this.$socket.open();
       } catch (error) {
-        this.error = error.message ? error.message : this.defaultError
+        this.error = error.message ? error.message : this.defaultError;
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style lang="scss">
@@ -96,10 +92,10 @@ export default {
   &__submit {
     width: 100%;
   }
-  &-error{
+  &-error {
     width: 100%;
     p {
-      color: red
+      color: red;
     }
   }
 }
