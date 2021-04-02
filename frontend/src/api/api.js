3ff55b280bcbb612ccf1f23ec87ed7ced396bbde
Park Sangwoo <tkddnsos@gmail.com>
2021-04-02 22:47:19 +0900

    S04P23B107-111 [feat] : FE - 회원 관련 환경 설정

---
+++ frontend/src/api/api.js
@@ -0,0 +1,15 @@
+export default {
+    URL: 'https://localhost:8080/api',
+    ROUTES: {
+ 
+      // accounts
+      myaccount: '/users',
+      signup: '/auth/signup',
+      social: '/auth/social',
+      login: '/auth/login',
+      password: '/auth/password',
+      changepassword: '/auth/resetPassword/update',
+         
+    }
+  }
+  
