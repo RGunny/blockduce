201764ad7e4469d978d01f26386c28f0677b8275
Park Sangwoo <tkddnsos@gmail.com>
2021-04-02 22:46:50 +0900

    S04P23B107-111 [feat] : FE - 회원관련 페이지 개발

--- frontend/src/views/accounts/Join.vue
+++ frontend/src/views/accounts/Join.vue
@@ -1,108 +1,315 @@
 <template>
-  <div>
-    <div>회원가입</div>
-    <div class="line"></div>
+  <div class="background">
+    <div class="container p-3 mt-5 bg-light-ivory signup-form">
+      <h3>회원가입</h3>
 
+      <div class="input-with-label">
+        <input
+          v-model="signupData.name"
+          v-bind:class="{
+            error: error.name,
+            complete: !error.name && signupData.name.length !== 0,
+          }"
+          class="inputs"
+          id="name"
+          placeholder="이름"
+          type="text"
+          autocapitalize="none"
+          autocorrect="none"
+          style="text-transform:lowercase"
+        />
+        <label for="name"></label>
+        <div class="error-text ml-3" v-if="error.name">
+          {{ error.name }}
+        </div>
+      </div>
 
-     <div class="outer">
-       <div class="inner">
+      <div class="input-with-label">
+        <input
+          v-model="signupData.email"
+          v-bind:class="{
+            error: error.email,
+            complete: !error.email && signupData.email.length !== 0,
+          }"
+          class="inputs"
+          id="email"
+          placeholder="이메일"
+          type="text"
+          autocapitalize="none"
+          autocorrect="none"
+          style="text-transform:lowercase"
+          required
+        />
+        <label for="email"></label>
+        <div class="error-text ml-3" v-if="error.email">{{ error.email }}</div>
+      </div>
 
-        <vs-input class="interval" v-model="value1" placeholder="Email">
-          <template v-if="validEmail" #message-success>
-            가능한 이메일입니다!
-          </template>
-          <template v-if="(!validEmail && value1 !== '') || value1 == ''" #message-danger>
-            이메일 형식으로 입력해주세요.
-          </template>
-        </vs-input>
+      <div class="input-with-label">
+        <input
+          v-model="signupData.password"
+          v-bind:class="{
+            error: error.password,
+            complete: !error.password && signupData.password.length !== 0,
+          }"
+          class="inputs"
+          id="password"
+          type="password"
+          placeholder="비밀번호를 입력하세요."
+          required
+        />
+        <label for="password"></label>
+        <div class="error-text ml-3" v-if="error.password">
+          {{ error.password }}
+        </div>
+      </div>
 
-        <vs-input class="interval" v-model="value2" placeholder="Name">
-          <template v-if="validName" #message-success>
-            멋진 이름이네요!
-          </template>
-          <template v-if="!validName || value2 === ''" #message-danger>
-            이름을 입력해주세요.
-          </template>
-        </vs-input>
-
-        <vs-input class="interval" type="password" v-model="value3" placeholder="Password">
-          <template v-if="validPassword" #message-success>
-            안전한 비밀번호입니다!
-          </template>
-          <template v-if="!validPassword || value3 === ''" #message-danger>
-            6~20자의 영문 대소문자<br>
-            최소 1개의 숫자 혹은 특수문자 포함
-          </template>
-        </vs-input>
-
-        <vs-input class="interval" type="password" v-model="value4" placeholder="ConfirmPassword">
-          <template v-if="!same" #message-danger>
-            비밀번호가 일치하지 않습니다.
-          </template>
-        </vs-input>
-
-        <vs-button class="intervalForButton" color="#7d33ff" flat>
-          회원가입
-        </vs-button>
-
-       </div>
+      <div class="input-with-label">
+        <input
+          v-model="signupData.passwordConfirm"
+          type="password"
+          id="password-confirm"
+          v-bind:class="{
+            error: error.passwordConfirm,
+            complete:
+              !error.passwordConfirm && signupData.passwordConfirm.length !== 0,
+          }"
+          placeholder="비밀번호를 다시 입력해주세요."
+          class="inputs"
+          required
+          @keyup.enter="clickSignup"
+        />
+        <label for="password-confirm"></label>
+        <div class="error-text ml-3" v-if="error.passwordConfirm">
+          {{ error.passwordConfirm }}
+        </div>
+      </div>
+      <div class="buttons mt-3">
+        <button
+          class="btn signup-button"
+          :class="{ disabled: !isSubmit }"
+          @click="clickSignup"
+        >
+          가입하기
+        </button>
+      </div>
+      <p class="my-3">
+        <span class="items" @click="toLogin">로그인하기</span>
+      </p>
     </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
+  name: 'Signup',
+  data() {
+    return {
+      signupData: {
+        email: '',
+        password: '',
+        passwordConfirm: '',
+        name: '',
+      },
+      error: {
+        email: false,
+        name: false,
+        password: false,
+        passwordConfirm: false,
+      },
+      isSubmit: false,
+    };
+  },
+  created() {
+    this.component = this;
+  },
+  watch: {
+    signupData: {
+      deep: true,
 
-   data:() => ({
-        value1: '',
-        value2: '',
-        value3: '',
-        value4: '',
-      }),
-      computed: {
-        validEmail() {
-          return /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/.test(this.value1)
-        },
-
-        validName() {
-          return /^[가-힣]+$/.test(this.value2)
-        },
-
-        validPassword() {
-          return /^(?=.*[a-zA-Z])((?=.*\d)|(?=.*\W)).{6,20}$/.test(this.value3)
-        },
-
-        same() {
-          if (this.value3 === this.value4 ) {
-            return true
-          }
-          else {
-            return false
-          }
-        }
-
+      handler() {
+        this.checknameForm();
+        this.checkEmailForm();
+        this.checkPasswordForm();
+        this.checkPasswordConfirmationForm();
+      },
+    },
+  },
+  methods: {
+    checknameForm() {
+      if (this.signupData.name.length > 0) {
+        this.error.name = false;
+      } else this.error.name = '이름을 입력해주세요.';
+    },
+    checkEmailForm() {
+      if (
+        this.signupData.email.length > 0 &&
+        !this.validEmail(this.signupData.email)
+      ) {
+        this.error.email = '올바른 이메일 형식이 아니에요';
+      } else this.error.email = false;
+    },
+    validEmail(email) {
+      // eslint-disable-next-line
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    },
+    checkPasswordForm() {
+      if (
+        this.signupData.password.length > 0 &&
+        this.signupData.password.length < 8
+      ) {
+        this.error.password = '비밀번호가 너무 짧아요';
+      } else if (
+        this.signupData.password.length >= 8 &&
+        !this.validPassword(this.signupData.password)
+      ) {
+        this.error.password = '영문, 숫자 포함 8 자리 이상이어야 해요.';
+      } else this.error.password = false;
+    },
+    validPassword(password) {
+      var va = /^(?=.*\d)(?=.*[a-z])(?=.*[a-zA-Z]).{8,}$/;
+      return va.test(password);
+    },
+    checkPasswordConfirmationForm() {
+      if (
+        this.signupData.password.length >= 8 &&
+        this.validPassword(this.signupData.password)
+      ) {
+        if (this.signupData.password !== this.signupData.passwordConfirm)
+          this.error.passwordConfirm = '비밀번호가 일치하지 않아요.';
+        else this.error.passwordConfirm = false;
       }
 
-}
+      // 버튼 활성화
+      if (
+        this.signupData.name.length > 0 &&
+        this.signupData.email.length > 0 &&
+        this.signupData.password.length > 0 &&
+        this.signupData.passwordConfirm.length > 0
+      ) {
+        let isSubmit = true;
+        Object.values(this.error).map((v) => {
+          if (v) isSubmit = false;
+        });
+        this.isSubmit = isSubmit;
+      }
+    },
+    clickSignup() {
+      if (this.isSubmit) {
+        this.signup(this.signupData);
+      }
+    },
+    toLogin() {
+      this.$router.push({ name: 'Login' });
+    },
+    ...mapActions('accountStore', ['signup']),
+  },
+};
 </script>
 
 <style scoped>
-  .outer {
-    text-align: center;
-    margin-top: 80px;
-  }
+.container {
+  width: 30%;
+  border-radius: 25px;
+}
 
-  .inner {
-    display: inline-block;
-  }
+h3 {
+  color: #3700ff;
+  font-weight: 800;
+}
 
-  .interval {
-    margin-top: 30px;
-  }
+.inputs {
+  border-style: none;
+  border-bottom: 1px solid #88a498;
+  background-color: transparent;
+  width: 80%;
+  padding: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  margin-top: 20px;
+}
 
-  .intervalForButton {
-    margin: 50px auto;
-  }
+.signup-button {
+  background-color: #3743ac;
+  color: #f8f8f8;
+  width: 70%;
+}
 
+.divide {
+  width: 10%;
+  border-top: 1px solid #88a498;
+  margin-left: auto;
+  margin-right: auto;
+}
 
+.kakao {
+  background-color: #ffe812;
+  border-radius: 5px;
+  width: 70%;
+}
+
+.google {
+  background-color: #ffffff;
+  border-radius: 5px;
+  width: 70%;
+}
+
+.inputs:focus {
+  border-style: none;
+  border-bottom: 2px solid #d6cbbd;
+  outline-style: none;
+}
+
+input[type='password'] {
+  font-family: sans-serif;
+}
+.error,
+.error:focus {
+  border-bottom: 2px solid rgb(250, 25, 59, 0.7);
+}
+
+.error-text {
+  color: rgb(250, 25, 59, 0.7);
+  text-align: left;
+  padding-left: 30px;
+}
+
+.signup-button:hover {
+  background-color: #0011ff;
+  color: #f8f8f8;
+}
+
+.disabled,
+.disabled:hover {
+  background-color: rgb(136, 154, 152, 0.25);
+  color: #f8f8f8;
+  cursor: inherit;
+}
+
+.background {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  height: 100vh;
+  background-image: url('https://user-images.githubusercontent.com/70404643/113249815-55915800-92fa-11eb-989b-e7c56b3df63e.PNG');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  background: url('https://user-images.githubusercontent.com/70404643/113249815-55915800-92fa-11eb-989b-e7c56b3df63e.PNG')
+    no-repeat center/cover;
+}
+
+.signup-form {
+  margin-top: 20vh !important;
+  opacity: 0.9;
+}
+
+.items:hover {
+  cursor: pointer;
+  color: #d6cbbd;
+}
 </style>
