f2a287b3b9ce7dd00df5c3d215bd3196eaaebdbe
youngjoon_multicampus <junjun0905@naver.com>
2021-04-07 14:19:42 +0900

    S04P23B107-140 [hotfix] : 지갑생성 시 초기eth 지급

--- backend/src/main/java/com/special/blockduce/transaction/service/TransactionService.java
+++ backend/src/main/java/com/special/blockduce/transaction/service/TransactionService.java
@@ -15,6 +15,8 @@ import com.special.blockduce.transaction.repository.DbcRepository;
 import com.special.blockduce.transaction.repository.EthRepository;
 import com.special.blockduce.utils.SaltUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.web3j.abi.FunctionEncoder;
@@ -23,10 +25,18 @@ import org.web3j.abi.datatypes.Bool;
 import org.web3j.abi.datatypes.Function;
 import org.web3j.abi.datatypes.generated.Uint256;
 import org.web3j.crypto.Credentials;
+import org.web3j.crypto.RawTransaction;
+import org.web3j.crypto.TransactionEncoder;
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
+import org.web3j.protocol.core.methods.response.EthGetTransactionReceipt;
 import org.web3j.protocol.core.methods.response.EthSendTransaction;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.RawTransactionManager;
+import org.web3j.utils.Convert;
+import org.web3j.utils.Numeric;
 
 import java.io.IOException;
 import java.math.BigInteger;
@@ -347,6 +357,10 @@ public class TransactionService {
         // 아이디 찾아오면 member로 넣고 못찾으면 ismem false로 바꿔서 넣고...
         Member member = memberRepository.findMemberByid(memberId).orElse(mem);
 
+        //어카운트 생성 시 eth  0.0999 지급
+        ethReward(memberId,333333333.0);
+        Double ethInit = 333333333.0*0.0000000003;
+
         if(member.getId() != null){ //계정이 있을 경우
 //            Account account = Account.builder().
 //                    key(form.getKey()).
@@ -357,7 +371,7 @@ public class TransactionService {
             member.getAccount().updateAccount(form.getAccount());
             member.getAccount().updateDbc(form.getDbc());
             member.getAccount().updateKey(form.getKey());
-            member.getAccount().updateEth(form.getEth());
+            member.getAccount().updateEth(ethInit);
             return "지갑 생성 완료";
         }
     return "지갑 생성 오류";
@@ -454,6 +468,105 @@ public class TransactionService {
     public void createEthTransaction(DbcEthDto EthTransactionInfo) {
         System.out.println("createEthTransaction 들어왔어");
         createRewardDbc(EthTransactionInfo);
+    }
+
+    public String ethReward(Long memberId, Double dbcvelue) {
+        String Account = findAccountByid(memberId);
+        double velue = 0.0000000003*dbcvelue;
+
+        Web3j web3 = Web3j.build(new HttpService(
+                "https://ropsten.infura.io/v3/b04025a46bb245b3bdb7c350a938dbe5"));
+
+        try{
+            String privetKey = "91b40449775898b8c31c8cb914f5408bc4e2a619cab888fcf1b0f823b8905ffd";
+            Credentials credentials = Credentials.create(privetKey);
+            System.out.println("recipientAddress: "+Account);
+
+            System.out.println(credentials.getAddress());
+            EthGetTransactionCount ethGetTransactionCount = web3
+                    .ethGetTransactionCount(credentials.getAddress(), DefaultBlockParameterName.LATEST).send();
+            BigInteger nonce = ethGetTransactionCount.getTransactionCount();
+
+            // Recipient address
+            String recipientAddress = Account;
+            // Value to transfer (in wei)
+
+            String tmp = Double.toString(velue);
+            String amountToBeSent= tmp; // 60원
+            BigInteger value = Convert.toWei(amountToBeSent, Convert.Unit.ETHER).toBigInteger();
+
+            // Gas Parameter
+            BigInteger gasLimit = BigInteger.valueOf(21000);
+            BigInteger gasPrice = Convert.toWei("1", Convert.Unit.GWEI).toBigInteger();
+
+            // Prepare the rawTransaction
+            RawTransaction rawTransaction = RawTransaction.createEtherTransaction(nonce, gasPrice, gasLimit,
+                    recipientAddress, value);
+
+            // Sign the transaction
+            byte[] signedMessage = TransactionEncoder.signMessage(rawTransaction, credentials);
+            String hexValue = Numeric.toHexString(signedMessage);
+
+            // Send transaction
+            EthSendTransaction ethSendTransaction = web3.ethSendRawTransaction(hexValue).send();
+            String transactionHash = ethSendTransaction.getTransactionHash();
+
+            System.out.println("transactionHash: " + transactionHash);
+            if(transactionHash == null){
+                return "사용자가 많아 처리가 지연되고 있습니다. 잠시후 이용해주세요";
+            }
+            // Wait for transaction to be mined
+            Optional<TransactionReceipt> transactionReceipt = null;
+            do {
+                EthGetTransactionReceipt ethGetTransactionReceiptResp = web3.ethGetTransactionReceipt(transactionHash)
+                        .send();
+                transactionReceipt = ethGetTransactionReceiptResp.getTransactionReceipt();
+                Thread.sleep(3000); // Wait for 3 sec
+            } while (!transactionReceipt.isPresent());
+
+            System.out.println("Transaction " + transactionHash + " was mined in block # "
+                    + transactionReceipt.get().getBlockNumber());
+            System.out.println("블록해시 : "
+                    + transactionReceipt.get().getBlockHash());
+            System.out.println("가스유즈드 : "
+                    + transactionReceipt.get().getGasUsed());
+            System.out.println("받는사람 주소 : "
+                    + Account);
+            System.out.println("주는사람 주소(관리자) : "
+                    + credentials.getAddress());
+            System.out.println("트렌젝션피 : "
+                    + transactionReceipt.get().getCumulativeGasUsed());
+            System.out.println("트렌젝션해시 : "
+                    + transactionReceipt.get().getTransactionHash());
+            System.out.println("보낸이더 : "
+                    + value);
+            System.out.println("보낸사람 아이디 : 후보자테이블 1번  보상 받는 맴버 아이디: "
+                    + memberId);
+            System.out.println("value: "
+                    + (0.0000000003*dbcvelue));
+
+            DbcEthDto EthTransactionInfo = DbcEthDto.builder().
+                    senderId(1L).
+                    receiverId(memberId).
+                    senderAccount(credentials.getAddress()).
+                    receiverAccount(Account).
+                    blockHash(transactionReceipt.get().getBlockHash()).
+                    value((long) (0.0000000003*dbcvelue)).
+                    transactionFee(transactionReceipt.get().getCumulativeGasUsed().longValue()).
+                    gasUsed(transactionReceipt.get().getGasUsed().longValue()).
+                    localDateTime(LocalDateTime.now()).
+                    blockNumber(transactionReceipt.get().getBlockNumber().longValue()).
+                    transactionHash(transactionReceipt.get().getTransactionHash()).
+                    isDbcEth(0L).
+                    build();
+            System.out.println("EthTransactionInfo의 isdbceth는?"+EthTransactionInfo.getIsDbcEth());
+            System.out.println("EthTransactionInfo의 isdbceth는?"+EthTransactionInfo.getIsDbcEth());
+            createEthTransaction(EthTransactionInfo);
+
+            return "eth 보상 지급완료";
+        } catch (IOException | InterruptedException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
 
